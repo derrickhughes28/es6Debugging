a1eeb540c0ceef5d5f4390b4bcc10a66fdcdec9a
derrick <derrick.hughes28@gmail.com>
2020-10-19 12:07:18 -0400

    fixed

---
+++ app.js
@@ -0,0 +1,44 @@
+var x = 5;
+var y = 6;
+
+
+function add(x, y){
+    return (x + y);
+};
+
+var result = add(x, y);
+
+console.log(result);
+
+
+
+function sub(x, y){
+    return (x - y);
+};
+
+var result = sub(x, y);
+
+console.log(result);
+
+
+function times(x, y){
+    return (x * y);
+};
+
+var result = times(x, y);
+
+console.log(result);
+
+
+function divide(x, y){
+    return (x / y);
+};
+
+var result = divide(x, y);
+
+console.log(result);
+
+
+
+var sum = (x,y) => (x + y);  
+console.log(sum(x,y));
